7b7cb24b2ec9db147fbcfbee1df27fbbc6700a8c
jmbaik <famian@daum.net>
2023-05-02 12:16:08 +0900

    material table

--- src/main.jsx
+++ src/main.jsx
@@ -16,15 +16,22 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@material-tailwind/react";
 import { MaterialTailwindControllerProvider } from "@/context";
 import "../public/css/tailwind.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RecoilRoot } from "recoil";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <MaterialTailwindControllerProvider>
-          <App />
-        </MaterialTailwindControllerProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <ThemeProvider>
+            <MaterialTailwindControllerProvider>
+              <App />
+            </MaterialTailwindControllerProvider>
+          </ThemeProvider>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
--- src/pages/intro/Intro.jsx
+++ src/pages/intro/Intro.jsx
@@ -1,129 +1,106 @@
-import MTable, {
-  AvatarCell,
-  SelectColumnFilter,
-  StatusPill,
-} from "@/components/table/MTable";
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 
-const getData = () => {
-  const data = [
-    {
-      name: "Jane Cooper",
-      email: "jane.cooper@example.com",
-      title: "Regional Paradigm Technician",
-      department: "Optimization",
-      status: "Active",
-      role: "Admin",
-      age: 27,
-      imgUrl:
-        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+import MaterialReactTable from "material-react-table";
+import { Button } from "@material-tailwind/react";
+
+const data = [
+  {
+    name: {
+      firstName: "John",
+      lastName: "Doe",
     },
-    {
-      name: "Cody Fisher",
-      email: "cody.fisher@example.com",
-      title: "Product Directives Officer",
-      department: "Intranet",
-      status: "Inactive",
-      role: "Owner",
-      age: 43,
-      imgUrl:
-        "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    address: "261 Erdman Ford",
+    city: "East Daphne",
+    state: "Kentucky",
+  },
+  {
+    name: {
+      firstName: "Jane",
+      lastName: "Doe",
     },
-    {
-      name: "Esther Howard",
-      email: "esther.howard@example.com",
-      title: "Forward Response Developer",
-      department: "Directives",
-      status: "Active",
-      role: "Member",
-      age: 32,
-      imgUrl:
-        "https://images.unsplash.com/photo-1520813792240-56fc4a3765a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    address: "769 Dominic Grove",
+    city: "Columbus",
+    state: "Ohio",
+  },
+  {
+    name: {
+      firstName: "Joe",
+      lastName: "Doe",
     },
-    {
-      name: "Jenny Wilson",
-      email: "jenny.wilson@example.com",
-      title: "Central Security Manager",
-      department: "Program",
-      status: "Offline",
-      role: "Member",
-      age: 29,
-      imgUrl:
-        "https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    address: "566 Brakus Inlet",
+    city: "South Linda",
+    state: "West Virginia",
+  },
+  {
+    name: {
+      firstName: "Kevin",
+      lastName: "Vandy",
     },
-    {
-      name: "Kristin Watson",
-      email: "kristin.watson@example.com",
-      title: "Lean Implementation Liaison",
-      department: "Mobility",
-      status: "Inactive",
-      role: "Admin",
-      age: 36,
-      imgUrl:
-        "https://images.unsplash.com/photo-1532417344469-368f9ae6d187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+    address: "722 Emie Stream",
+    city: "Lincoln",
+    state: "Nebraska",
+  },
+  {
+    name: {
+      firstName: "Joshua",
+      lastName: "Rolluffs",
     },
-    {
-      name: "Cameron Williamson",
-      email: "cameron.williamson@example.com",
-      title: "Internal Applications Engineer",
-      department: "Security",
-      status: "Active",
-      role: "Member",
-      age: 24,
-      imgUrl:
-        "https://images.unsplash.com/photo-1566492031773-4f4e44671857?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-    },
-  ];
-  return [...data, ...data, ...data];
-};
+    address: "32188 Larkin Turnpike",
+    city: "Charleston",
+    state: "South Carolina",
+  },
+];
 
 const Intro = () => {
+  const [rowSelection, setRowSelection] = useState({});
   const columns = useMemo(
     () => [
       {
-        Header: "Name",
-        accessor: "name",
-        Cell: AvatarCell,
-        imgAccessor: "imgUrl",
-        emailAccessor: "email",
+        accessorKey: "name.firstName", //access nested data with dot notation
+        header: "First Name",
       },
       {
-        Header: "Title",
-        accessor: "title",
+        accessorKey: "name.lastName",
+        header: "Last Name",
       },
       {
-        Header: "Status",
-        accessor: "status",
-        Cell: StatusPill,
+        accessorKey: "address", //normal accessorKey
+        header: "Address",
       },
       {
-        Header: "Age",
-        accessor: "age",
+        accessorKey: "city",
+        header: "City",
       },
       {
-        Header: "Role",
-        accessor: "role",
-        Filter: SelectColumnFilter, // new
-        filter: "includes",
+        accessorKey: "state",
+        header: "State",
       },
     ],
     []
   );
 
-  const data = useMemo(() => getData(), []);
   return (
-    <div className="bg-gray-100 text-gray-900">
-      <main className="mx-auto px-4 pt-4 sm:px-6 lg:px-8">
-        <div className="">
-          <h1 className="text-xl font-semibold">
-            React Table + Tailwind CSS = ❤
-          </h1>
-        </div>
-        <div className="mt-6">
-          <MTable columns={columns} data={data} />
-        </div>
-      </main>
-    </div>
+    <>
+      <Button
+        onClick={() => {
+          const keys = Object.keys(rowSelection);
+          if (keys.length) {
+            console.log(keys[0]);
+          }
+        }}
+      >
+        삭제
+      </Button>
+      <MaterialReactTable
+        columns={columns}
+        data={data}
+        enableRowSelection
+        getRowId={(row) => row.name.firstName}
+        enableMultiRowSelection={false}
+        onRowSelectionChange={setRowSelection}
+        state={{ rowSelection }}
+      />
+    </>
   );
 };
 
